5bbca0880a429757085ed0fbb31e911e3a70d9df
wujingci-code <wujingci0202@gmail.com>
2025-07-29 20:00:51 +0800

    cel type

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"reflect"
+
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/ext"
 	"github.com/wujingci-code/rule_demo/cel_demo"
 	"github.com/wujingci-code/rule_demo/expr_demo"
 	"github.com/wujingci-code/rule_demo/perf_test"
 )
 
-func main() {
+type TestStruct struct {
+	Field string
+}
 
+func main() {
+	fmt.Println("Type name:", reflect.TypeOf(TestStruct{}).String())
+
+	_, err := cel.NewEnv(
+		ext.Strings(),
+		ext.Lists(),
+		cel.Types(&TestStruct{}),
+		cel.Variable("test", cel.ObjectType(reflect.TypeOf(TestStruct{}).String())),
+	)
+	if err != nil {
+		log.Fatalf("CEL 环境创建失败: %v", err)
+	}
 	expr_demo.Expr()
 	cel_demo.EVM()
 	cel_demo.UTXO()
